94e4e49c7086130aee8ab523338db9a5ce433322
wooseok-dev <wooseok-dev@gmail.com>
2025-08-30 19:41:28 +0900

    fix file

--- projects/fuzz-test/build.sh
+++ projects/fuzz-test/build.sh
@@ -1,3 +1,7 @@
+echo $PWD
+echo $SRC
+ls -alh
+find / -name "ini.c" 2> /dev/null
 $CC $CFLAGS $SANITIZER_FLAGS -c $SRC/fuzz-test/ini.c -I.
 $CC $CFLAGS $SANITIZER_FLAGS -c $SRC/fuzz.c -I.
 $CC $CFLAGS $SANITIZER_FLAGS $LIB_FUZZING_ENGINE ini.o fuzz.o -o $OUT/fuzz
